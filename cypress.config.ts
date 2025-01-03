684b2ca1152712be2c3102b58427bb5aaf5f44f7
Alberto Caparrós Trigo <Alberto.Caparros.Trigo@unit4.com>
2025-01-03 19:59:13 +0100

    fix lint errors

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4200',
-    setupNodeEvents(on, config) {
+    setupNodeEvents() {
       // Add custom setup here
     },
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
